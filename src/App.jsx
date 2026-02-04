334e108c12485f08f156ec0b9a9298cc347a9d7b
RabieGhabra <rabie@valus.io>
2026-02-04 17:10:56 +0400

    prepare for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,22 @@ import Home from './pages/Home/Home.jsx';
 import ShopByConcern from './pages/ShopByConcern.jsx';
 import ProductListing from './pages/ProductListing_page/ProductListing.jsx';
 import ScrollToTop from "./components/ScrollToTop";
+
 function App() {
   return (
-    <div>
-      <Router.BrowserRouter>
-        <ScrollToTop />
-        <Router.Routes>
-          <Router.Route path="/" element={<Home />} />
-          <Router.Route path="/Login" element={<Login />} />
-          <Router.Route path="/Otp" element={<Otp />} />
-          <Router.Route path="/Registration" element={<Registration />} />
-          <Router.Route path="/ShopByConcern" element={<ShopByConcern />} />
-          <Router.Route path="/ProductListing" element={<ProductListing />} />
-        </Router.Routes>
-      </Router.BrowserRouter>
-    </div>
+    <Router.BrowserRouter basename="/zahrat-al-rawdah-website">
+      <ScrollToTop />
+      <Router.Routes>
+        <Router.Route path="/" element={<Home />} />
+        <Router.Route path="/Login" element={<Login />} />
+        <Router.Route path="/Otp" element={<Otp />} />
+        <Router.Route path="/Registration" element={<Registration />} />
+        <Router.Route path="/ShopByConcern" element={<ShopByConcern />} />
+        <Router.Route path="/ProductListing" element={<ProductListing />} />
+      </Router.Routes>
+    </Router.BrowserRouter>
   );
 }
+
 export default App;
+
